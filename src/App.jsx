ed88c003e8028155bb519392df00c079250feadd
VoidOfMaya <mayaofthevoid@gmail.com>
2026-03-01 11:11:20 +0200

    feat: fetches data from blog server

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [blogData, setBlogData] = useState();
+  const [loading, setLoading] = useState(true);
+    useEffect(()=>{
+    fetch('https://blog-api-vdtu.onrender.com')
+    .then(response=>{
+      if(response.status >= 400) {
+          throw new Error('A server error has occured error code: ' + response.status )
+      }
+      return response.json();
+      })
+    .then( data =>{
+      setBlogData(data)
+    })
+    .catch(error => console.error(error))
+    .finally(()=> {setLoading(false)});
+
+
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Blogies</h1>
+      
     </>
   )
 }
